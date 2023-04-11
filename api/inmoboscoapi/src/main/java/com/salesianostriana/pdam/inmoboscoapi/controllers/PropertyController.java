358e7ad6122ee3eaaf0cdb94bf19ccddc53efea2
davidgm26 <davidgama260402@gmail.com>
2023-04-11 20:48:47 +0200

    controllers creados vacios

---
+++ api/inmoboscoapi/src/main/java/com/salesianostriana/pdam/inmoboscoapi/controllers/PropertyController.java
@@ -0,0 +1,15 @@
+package com.salesianostriana.pdam.inmoboscoapi.controllers;
+
+import com.salesianostriana.pdam.inmoboscoapi.services.PropertyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/inmueble")
+public class PropertyController {
+
+    private final PropertyService propertyService;
+
+}
---
+++ api/inmoboscoapi/src/main/java/com/salesianostriana/pdam/inmoboscoapi/controllers/UserController.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.pdam.inmoboscoapi.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+
+
+
+}
