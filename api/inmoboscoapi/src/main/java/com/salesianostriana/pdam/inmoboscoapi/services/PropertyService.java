cc703d93ecb51285985889b85a227a650ec43d62
davidgm26 <davidgama260402@gmail.com>
2023-04-11 21:59:39 +0200

    inicio de la seguridad

--- api/inmoboscoapi/src/main/java/com/salesianostriana/pdam/inmoboscoapi/services/PropertyService.java
+++ api/inmoboscoapi/src/main/java/com/salesianostriana/pdam/inmoboscoapi/services/PropertyService.java
@@ -1,9 +1,15 @@
 package com.salesianostriana.pdam.inmoboscoapi.services;
 
+import com.salesianostriana.pdam.inmoboscoapi.repositories.PropertyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class PropertyService {
+
+    private final PropertyRepository propertyRepository;
+
+
+
 }
