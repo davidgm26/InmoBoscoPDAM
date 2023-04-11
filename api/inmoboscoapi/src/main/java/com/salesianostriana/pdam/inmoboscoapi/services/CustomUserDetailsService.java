50ed567736173358183ccd9c9d74612275e9a08f
davidgm26 <davidgama260402@gmail.com>
2023-04-11 20:39:30 +0200

    servicios antiguos creados

---
+++ api/inmoboscoapi/src/main/java/com/salesianostriana/pdam/inmoboscoapi/services/CustomUserDetailsService.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.pdam.inmoboscoapi.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service("userDetailsService")
+@RequiredArgsConstructor
+
+public class CustomUserDetailsService {
+
+
+
+}
