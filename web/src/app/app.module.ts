eb5a1fc958ff47c66ed2c8c92574368069ec4b9f
davidgm26 <davidgama260402@gmail.com>
2023-05-29 18:02:53 +0200

    se crea el componente de registro y de login

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
