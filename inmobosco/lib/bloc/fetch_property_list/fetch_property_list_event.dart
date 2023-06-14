6c965971356b6512cacfc85745aa45e563cc7d3f
davidgm26 <davidgama260402@gmail.com>
2023-06-14 23:51:38 +0200

    update

--- inmobosco/lib/bloc/fetch_property_list/fetch_property_list_event.dart
+++
@@ -1,4 +0,0 @@
-part of 'fetch_property_list_bloc.dart';
-
-@immutable
-abstract class FetchPropertyListEvent {}
--- inmobosco2/lib/repository/property-repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:inmobosco2/models/property_response.dart';
-
-abstract class PropertyRepository {
-  Future<PropertyResponse> getAllProperties(int numPage,String token);
-  Future<PropertyResponse> getOneProperty(int id, String token);
-
-}
